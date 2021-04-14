dbf599f59a7870305dadd20a721d24cbfff0439b
Mariana Forero Avila <micorreo@uniandes.edu.co>
2021-04-14 14:20:17 -0500

    para

--- Entrenamieto 2.3.py
+++ Entrenamieto 2.3.py
@@ -4,25 +4,42 @@ import pulp as lp
 
 # CONJUNTOS
 
-LOCALES=['Chapinero','La Candelaria','Av 7','Av Jiménez']
+PELICULAS=['Titanic','Inception','Pulp Fiction','Fight club']
 
-UNIVERSIDADES=['Javeriana','Rosario','Tadeo','Central']
+PARQUEADEROS=['Oro','Platino','Plata','Bronce','VIP']
+
+FRANJAS=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
 
 # PARAMETROS
 # Cantidad maxima de panaderos un local
-panaderos={'Chapinero':20,'La Candelaria':15,'Av 7':10,'Av Jiménez':5}
+duracion={'Titanic':7,'Inception':6,'Pulp Fiction':6,'Fight club':5}
 
 #costo diario de mantenimiento [COP]   en un local 
-costo_mantenimiento={'Chapinero':250000,'La Candelaria': 165000 ,'Av 7':300000,'Av Jiménez':150000}
+min_proyecciones={'Titanic':1,'Inception':5,'Pulp Fiction':2,'Fight club':3}
 
 #Costo de envio de un local a una universidad
-costo_envio={('Chapinero','Javeriana'):0.3,('Chapinero','Rosario'):1.5,('Chapinero','Tadeo'):0.9,
-   ('Chapinero','Central'):0.4,('La Candelaria','Javeriana'):1.1,('La Candelaria','Rosario'):0.4,
-   ('La Candelaria','Tadeo'):0.2,('La Candelaria','Central'):1,('Av 7','Javeriana'):0.1,
-   ('Av 7','Rosario'):1.7,('Av 7','Tadeo'):1.9,('Av 7','Central'):0.2,
-   ('Av Jiménez','Javeriana'):1.7,('Av Jiménez','Rosario'):0.1,('Av Jiménez','Tadeo'):0.3,
-   ('Av Jiménez','Central'):1.6}
+asistencia={('Titanic',1):73,('Titanic',2):107,('Titanic',3):96,
+   ('Titanic',4):78,('Titanic',5):75,('Titanic',6):54,
+   ('Titanic',7):105,('Titanic',8):62,('Titanic',9):110,
+   ('Titanic',10):115,('Titanic',11):0,('Titanic',12):0,
+   ('Titanic',13):0,('Titanic',14):0,('Titanic',15):0,
+   ('Titanic',16):0,('Inception',1):102,('Inception',2):109,('Inception',3):128,
+   ('Inception',4):108,('Inception',5):115,('Inception',6):113,
+   ('Inception',7):119,('Inception',8):129,('Inception',9):128,
+   ('Inception',10):126,('Inception',11):117,('Inception',12):0,
+   ('Inception',13):0,('Inception',14):0,('Inception',15):0,
+   ('Inception',16):0, ('Pulp Fiction',1):79,('Pulp Fiction',2):118,('Pulp Fiction',3):95,
+   ('Pulp Fiction',4):107,('Pulp Fiction',5):123,('Pulp Fiction',6):121,
+   ('Pulp Fiction',7):110,('Pulp Fiction',8):94,('Pulp Fiction',9):105,
+   ('Pulp Fiction',10):127,('Pulp Fiction',11):111,('Pulp Fiction',12):0,
+   ('Pulp Fiction',13):0,('Pulp Fiction',14):0,('Pulp Fiction',15):0,
+   ('Pulp Fiction',16):0,('Fight club',1):98,('Fight club',2):137,('Fight club',3):147,
+   ('Fight club',4):96,('Fight club',5):92,('Fight club',6):108,
+   ('Fight club',7):137,('Fight club',8):107,('Fight club',9):138,
+   ('Fight club',10):133,('Fight club',11):89,('Fight club',12):119,
+   ('Fight club',13):0,('Fight club',14):0,('Fight club',15):0,
+   ('Fight club',16):0}
 
 #Demanda diaria en una Univerisdad
 demanda={'Javeriana':2745,'Rosario':3965,'Tadeo':4402,'Central':2003}
